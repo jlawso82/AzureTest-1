b7566bc9fc68cab5dbd62b96c48a710517948501
Rheanne <rheannemcintosh@gmail.com>
2016-04-15 15:46:30 +0100

    Changed from alive to changed

--- MIXITUP/script.js
+++ MIXITUP/script.js
@@ -1,3 +1,32 @@
 /**
  * Created by 1403557 on 15/04/2016.
  */
+function getResultsFromAbsolute () {
+    $.ajax({
+        type: "GET",
+        url: "http://addb.absolutdrinks.com/ingredients/?apiKey=358923c93ff0436bb76cb445300b70f1",
+        dataType: 'jsonp',
+        success: function(response){
+            console.log(response);
+
+        }
+    });
+}
+
+function addResultList(response) {
+    //create a string to contain our HTML code to inject
+    var htmlstring = "";
+    //iterate over the collection of results
+    for (var i=0; i<10; i++){
+        var title = jsondata.response[i].name;
+        htmlstring += "<li>" + name + "</li>";
+    }
+
+    //inject the HTML into our empty list
+    $("#helpmelist").html(htmlstring);
+}
+
+
+
+
+
