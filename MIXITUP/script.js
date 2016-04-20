8af73127fa420892b20ceb77fbffb9517b313915
Rheanne <rheannemcintosh@gmail.com>
2016-04-20 16:55:02 +0100

    Changed from alive to changed

--- MIXITUP/script.js
+++ MIXITUP/script.js
@@ -12,7 +12,7 @@ function getResultsFromAbsolute () {
             var htmlstring = "";
             //iterate over the collection of results
             var drinks=response["result"];
-;            for (var i in drinks)
+            for (var i in drinks)
             {
             var x=drinks[i];
                var title = x["name"];
@@ -29,6 +29,59 @@ function getResultsFromAbsolute () {
         }
     });
 }
+
+function mixItUp(){
+
+    var vodkaBox = $("#Vodka").is(":checked");
+    var ginBox = $("#Gin").is(":checked");
+    var sambucaBox = $("#Sambuca").is(":checked");
+    var x;
+
+    if(vodkaBox == true) {
+         x = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=wine&callback=?";
+    }
+
+    return x;
+}
+
+function getResults(){
+
+    var urlToUse = mixItUp();
+
+    $.ajax({
+        type: "GET",
+        url: urlToUse,
+        dataType: 'jsonp',
+        success: function(response){
+            callback(response);
+            //console.log(response);
+
+            //var htmlstring = "";
+            //iterate over the collection of results
+            //var drinks=response["result"];
+            //for (var i in drinks)
+            //{
+                //var x=drinks[i];
+                //var title = x["name"];
+                //htmlstring += "<li>" + title + "</li>";
+            //}
+            //console.log(htmlstring);
+            //for (var i = 0; i < 10; i++) {
+            //    var title = jsondata.response[i].name;
+            //    htmlstring += "<li>" + title + "</li>";
+            //}
+
+            //inject the HTML into our empty list
+            //$("#helpmelist").append(htmlstring);
+        }
+    });
+
+
+    function callback(jsonObj){
+        console.log(jsonObj);
+    }
+}
+
 
 
 
